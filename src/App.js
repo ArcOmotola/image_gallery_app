c4a329d256a7b76c6a872afbc12725a3ea548d69
Omotola Ogunsola <ogunsola40@gmail.com>
2022-04-23 23:43:12 +0100

    added search functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
   const [images, setImages] = useState([]);
@@ -7,24 +8,28 @@ function App() {
   const [term, setTerm] = useState('');
 
   useEffect(() => {
-    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`)
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
       .then(res => res.json())
       .then(data => {
         setImages(data.hits);
         setIsLoading(false);
       })
       .catch(err => console.log(err));
-  }, []);
+  }, [term]);
 
   // console.log("iiiidssss>>", images.id)
 
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-3 gap-4">
+      <ImageSearch searchText={(text) => setTerm(text)}/>
+
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center max-auto mt-32">No Images Found</h1> }
+
+      {isLoading ? <h1 className="text-6xl text-center max-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
         {images.map(image => (
           <ImageCard key={image.id} image={image}/>
         ))}
-      </div>
+      </div>}
     </div>
   );
 }
