6d3db73f243a884e68d37a29ae3a30a83d318fcf
Omotola Ogunsola <ogunsola40@gmail.com>
2022-04-23 22:13:52 +0100

    fetched dynamic images from pixabay API

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,31 @@
-const tailwindcss = require('tailwindcss');
+// const tailwindcss = require('tailwindcss');
+
+// module.exports = {
+//     plugins: [
+//         tailwindcss('./tailwind.js'),
+//         require('autoprefixer')
+//     ],
+//         content: [
+//             "./src/**/*.{js,jsx,ts,tsx}",
+//         ],
+// }
+
+
+// postcss.config.js
+
+// module.exports = {
+//     plugins: {
+//         'postcss-import': {},
+//         tailwindcss: {},
+//         autoprefixer: {},
+//     }
+// }
+
 
 module.exports = {
-    plugins: [
-        tailwindcss('./tailwind.js'),
-        require('autoprefixer')
-    ],
-        content: [
-            "./src/**/*.{js,jsx,ts,tsx}",
-        ],
-}
-
-
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  }
+  
